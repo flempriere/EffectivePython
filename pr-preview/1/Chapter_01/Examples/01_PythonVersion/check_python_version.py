cd77a0d49cd8e8301bacf7ddcfc267e9093145e7
flempriere <flempriere@users.noreply.github.com>
2026-03-13 14:17:33 +0000

    deploy: 51b5f80b3418aee6cf1e545b2e251935cabf4e38

---
+++ pr-preview/1/Chapter_01/Examples/01_PythonVersion/check_python_version.py
@@ -0,0 +1,28 @@
+"""
+Item 1: Check Python Version.
+
+Uses the `sys` module to dynamically interrogate the python version
+
+Based on original code developed by Brett Slatkin
+
+Copyright 2014-2024 Brett Slatkin, Pearson Education Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import sys
+
+print(sys.platform)
+print(sys.implementation.name)
+print(sys.version_info)
+print(sys.version)
